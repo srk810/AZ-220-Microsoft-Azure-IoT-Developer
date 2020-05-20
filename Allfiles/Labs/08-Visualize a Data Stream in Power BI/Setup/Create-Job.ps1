b805102b1bee5ee9867009badc65e7fda99289b1
Daren May <daren.may@gmail.com>
2020-05-19 18:28:56 -0600

    lab08 - updated setup scripts and created zip

--- Allfiles/Labs/08-Visualize a Data Stream in Power BI/Setup/Create-Job.ps1
+++ Allfiles/Labs/08-Visualize a Data Stream in Power BI/Setup/Create-Job.ps1
@@ -29,6 +29,8 @@ param(
     $ScriptLocation
 )
 
+$ErrorActionPreference= 'silentlycontinue'
+
 # Set the current Subcription to the one used in BASH
 Get-AzSubscription -SubscriptionId $SubscriptionID | Select-AzSubscription >> build.log
 
@@ -39,12 +41,30 @@ $jobDefinition.location = $Location
 $jobDefinition | ConvertTo-Json | set-content $pathToJson
 
 $jobName = "vibrationJob"
-Write-Host "Creating job" $jobName -ForegroundColor DarkYellow
-$job = New-AzStreamAnalyticsJob `
+Write-Host "Creating job" $jobName "- " -ForegroundColor DarkYellow -NoNewLine
+$exists= Get-AzStreamAnalyticsJob `
   -ResourceGroupName $ResourceGroup `
-  -File $pathToJson `
-  -Name $jobName `
-  -Force 
+  -Name $jobName
+
+if ($null -eq $exists) {
+  $job = New-AzStreamAnalyticsJob `
+    -ResourceGroupName $ResourceGroup `
+    -File $pathToJson `
+    -Name $jobName `
+    -Force 
+  if ($job.ProvisioningState -eq "Succeeded")
+  {
+    Write-Host $job.ProvisioningState -ForegroundColor DarkGreen
+  }
+  else
+  {
+    Write-Host $job.ProvisioningState -ForegroundColor DarkRed
+  }
+}
+else {
+  Write-Host "Already exists" -ForegroundColor DarkGreen
+}
+
 
 # Update JobInputDefinition
 $pathToJson = $ScriptLocation + "/JobInputDefinition.json"
@@ -53,12 +73,32 @@ $jobInputDefinition.properties.datasource.properties.iotHubNamespace = $IoTHubNa
 $jobInputDefinition | ConvertTo-Json -Depth 10 | set-content $pathToJson
 
 $jobInputName = "vibrationInput"
-Write-Host "Creating job input" $jobInputName  -ForegroundColor DarkYellow
-$jobInput = New-AzStreamAnalyticsInput `
-  -ResourceGroupName $resourceGroup `
+Write-Host "Creating job input" $jobInputName "- " -ForegroundColor DarkYellow -NoNewLine
+
+$exists= Get-AzStreamAnalyticsInput `
+  -ResourceGroupName $ResourceGroup `
   -JobName $jobName `
-  -File $pathToJson `
   -Name $jobInputName 
+
+if ($null -eq $exists) {
+  $jobInput = New-AzStreamAnalyticsInput `
+    -ResourceGroupName $resourceGroup `
+    -JobName $jobName `
+    -File $pathToJson `
+    -Name $jobInputName 
+  if ($jobInput.Name -eq $jobInputName)
+  {
+    Write-Host "Succeeded" -ForegroundColor DarkGreen
+  }
+  else
+  {
+    Write-Host "Failed" -ForegroundColor DarkRed
+  }
+}
+else {
+  Write-Host "Already exists" -ForegroundColor DarkGreen
+}
+
 
 # Update JobOutputDefinition
 $pathToJson = $ScriptLocation + "/JobOutputDefinition.json"
@@ -69,20 +109,57 @@ $JobOutputDefinition.properties.datasource.properties.container = $Container
 $JobOutputDefinition | ConvertTo-Json -Depth 10 | set-content $pathToJson
 
 $jobOutputName = "vibrationOutput"
-Write-Host "Creating job output" $jobOutputName -ForegroundColor DarkYellow
-$jobOutput = New-AzStreamAnalyticsOutput `
-  -ResourceGroupName $resourceGroup `
+Write-Host "Creating job output" $jobOutputName "- " -ForegroundColor DarkYellow -NoNewLine
+
+$exists= Get-AzStreamAnalyticsOutput `
+  -ResourceGroupName $ResourceGroup `
   -JobName $jobName `
-  -File $pathToJson `
-  -Name $jobOutputName -Force 
+  -Name $jobOutputName 
+
+if ($null -eq $exists) {
+  $jobOutput = New-AzStreamAnalyticsOutput `
+    -ResourceGroupName $resourceGroup `
+    -JobName $jobName `
+    -File $pathToJson `
+    -Name $jobOutputName
+  if ($jobOutput.Name -eq $jobOutputName)
+  {
+    Write-Host "Succeeded" -ForegroundColor DarkGreen
+  }
+  else
+  {
+    Write-Host "Failed" -ForegroundColor DarkRed
+  }
+}
+else {
+  Write-Host "Already exists" -ForegroundColor DarkGreen
+}
 
 # Create Query
 $jobTransformationName = "VibrationJobTransformation"
 $pathToJson = $ScriptLocation + "/JobTransformationDefinition.json"
-Write-Host "Creating job transformation" $jobTransformationName -ForegroundColor DarkYellow
-$jobTransformation = New-AzStreamAnalyticsTransformation `
-  -ResourceGroupName $resourceGroup `
+Write-Host "Creating job transformation" $jobTransformationName "- " -ForegroundColor DarkYellow -NoNewLine
+
+$exists= Get-AzStreamAnalyticsTransformation `
+  -ResourceGroupName $ResourceGroup `
   -JobName $jobName `
-  -File $pathToJson `
-  -Name $jobTransformationName -Force 
-  
+  -Name $jobTransformationName
+
+if ($null -eq $exists) {
+  $jobTransformation = New-AzStreamAnalyticsTransformation `
+    -ResourceGroupName $resourceGroup `
+    -JobName $jobName `
+    -File $pathToJson `
+    -Name $jobTransformationName
+  if ($jobTransformation.Name -eq $jobTransformationName)
+  {
+    Write-Host "Succeeded" -ForegroundColor DarkGreen
+  }
+  else
+  {
+    Write-Host "Failed" -ForegroundColor DarkRed
+  }
+}
+else {
+  Write-Host "Already exists" -ForegroundColor DarkGreen
+}
