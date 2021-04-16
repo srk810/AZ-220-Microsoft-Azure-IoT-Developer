82a5ab77b1178a00e034b7d42a9a8df5d72447aa
Daren May <daren.may@gmail.com>
2021-04-16 13:22:14 -0600

    debugging script

--- Allfiles/Bicep/create-device.sh
+++ Allfiles/Bicep/create-device.sh
@@ -1,16 +1,16 @@
-# IoTHubName=$1
-# DeviceID=$2
+IoTHubName=$1
+DeviceID=$2
 
-exists=$( az iot hub device-identity list  --hub-name $1 --query "[?contains(deviceId, '$2')].deviceId" -o tsv )
+exists=$( az iot hub device-identity list  --hub-name $IoTHubName --query "[?contains(deviceId, '${DeviceID}')].deviceId" -o tsv )
 if [ "${exists}" == "${DeviceID}" ]
 then
     echo "Already exists"
 else
-    output=$( az iot hub device-identity create --hub-name $1 --device-id $2-o json >> build.log 2>&1 )
+    output=$( az iot hub device-identity create --hub-name $IoTHubName --device-id $DeviceID -o json >> build.log 2>&1 )
 fi
 
-DeviceConnectionString=$( az iot hub device-identity connection-string show --hub-name $1 --device-id $2 -o tsv )
+DeviceConnectionString=$( az iot hub device-identity connection-string show --hub-name $IoTHubName --device-id $DeviceID -o tsv )
 
 echo $1
 echo $2
-echo "{'DeviceConnectionString':'${DeviceConnectionString}', 'Arg1': '$1', 'Arg2': '$2'}" > $AZ_SCRIPTS_OUTPUT_PATH
+echo "{'DeviceConnectionString':'${DeviceConnectionString}', 'Arg1': '$1', 'Arg2': '$2'}"  $AZ_SCRIPTS_OUTPUT_PATH
